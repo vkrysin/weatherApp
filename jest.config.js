21f7584887bbcd00804586d40dfc7f47981eaed1
Vladislav Krysin <krysin18@mail.ru>
2021-02-24 15:18:30 +0300

    test: upd settings for travis

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    "src/**/*.{js,vue}",
-    "!**/node_modules/**"
-  ],
-  coverageReporters: [
-    "html",
-    "text-summary"
-  ]
+    preset: '@vue/cli-plugin-unit-jest',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.{js,vue}",
+        "!**/node_modules/**"
+    ],
+    coverageReporters: [
+        "html",
+        "text-summary"
+    ]
 }
